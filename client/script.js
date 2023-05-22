7a1d4f0b0a21573e1f7aba9baa9eaf455af8a713
gazua300 <mesmo245@gmail.com>
2023-05-22 01:56:27 -0300

    primeiro commit do projeto fullstack

---
+++ client/script.js
@@ -0,0 +1,188 @@
+//Sex orientation variables
+const homo = document.getElementById('homo')
+const hetero = document.getElementById('hetero')
+const bissex = document.getElementById('bissex')
+const assex = document.getElementById('assex')
+const pansex = document.getElementById('pansex')
+const other = document.getElementById('other-orientation')
+var selectedGenre = document.querySelectorAll('input[name="genre"]')
+let sexOrientation
+// Contact information
+const userName = document.getElementById('name')
+const tel = document.getElementById('whatsapp')
+const email = document.getElementById('email')
+const msg = document.getElementById('msg')
+const popup = document.querySelector('.password-modal')
+// Add info variables
+var selectedMusicStyles = document.querySelectorAll('input[name="musics"]')
+var selectedSports = document.querySelectorAll('input[name="sports"]')
+let musicStyles = []
+let sportsModalities = []
+
+
+
+
+const openList = ()=>{
+    document.querySelector('.popup').style.display = 'block'
+    showUserList()
+}
+
+const closeList = ()=>{
+    document.querySelector('.popup').style.display = 'none'
+}
+
+
+const usersStored = JSON.parse(localStorage.getItem('users'))
+
+const removeUser = (id)=>{
+    const decide = window.confirm('Tem certeza que deseja deletar esse usuário?')
+
+    if(decide){
+        fetch(`http://localhost:3003/user/${id}`, {
+            method:'DELETE'
+        }).then(res => res.text()).then(()=>{
+            showUserList()
+        }).catch(e=>{
+            alert(e.message)
+        })
+    }
+}
+
+const showUserList = ()=>{
+    fetch('http://localhost:3003/users').then(res=>{
+            if(res.ok){
+                return res.json()
+            }else{
+                return res.text()
+            }
+        }).then(data=>{   
+            document.querySelector('.popup-firstContents')
+                .innerHTML = data && data.map(user=>{
+                return`
+                    <div class='card'>
+                        <div>
+                            <div style='text-align:center;'>
+                                <b>${user.name}</b>
+                            </div>
+                            <p>Preferências musicais:<br>
+                            ${JSON.parse(user.music).map(music =>' '+music)}</p>
+                            <p>Esportes:<br>
+                            ${JSON.parse(user.sports).map(sport => ' '+sport)}</p>
+                            Orientação sexual: ${user.genre}
+                        </div>
+                        <div style='display:flex;justify-content:center;'>
+                        <div onclick="removeUser('${user.id}')"
+                            class='button'>Remover</div> 
+                        </div>               
+                    </div>
+                `
+            }).join('')
+        }).catch(e=>{
+            alert(e.message)
+    })
+}
+
+
+Array.from(selectedGenre).forEach(function(genre){
+    genre.addEventListener('change', function(){
+        if(genre.checked){
+            other.setAttribute('disabled', 'disabled')
+            sexOrientation = genre.value
+        }
+    })
+})
+
+Array.from(selectedMusicStyles).forEach(function(style){
+    style.addEventListener('change', function(){
+        if(style.checked){
+            musicStyles.push(style.value)
+        }
+    })
+})
+Array.from(selectedSports).forEach(function(sport){
+    sport.addEventListener('change', function(){
+        if(sport.checked){
+            sportsModalities.push(sport.value)
+        }
+    })
+})
+
+
+const enableRadios = ()=>{
+    homo.removeAttribute('disabled')
+    hetero.removeAttribute('disabled')
+    bissex.removeAttribute('disabled')
+    assex.removeAttribute('disabled')
+    pansex.removeAttribute('disabled')
+}
+
+other.addEventListener('input', ()=>{
+    pansex.setAttribute('disabled', 'disabled')
+    assex.setAttribute('disabled', 'disabled')
+    bissex.setAttribute('disabled', 'disabled')
+    hetero.setAttribute('disabled', 'disabled')
+    homo.setAttribute('disabled', 'disabled')   
+    
+    if(!other.value){
+        enableRadios()
+    }
+})
+
+document.getElementById('enable').addEventListener('click', ()=>{
+    other.removeAttribute('disabled')
+})
+
+document.getElementById('clear').addEventListener('click', ()=>{
+    other.value = null
+    enableRadios()
+})
+
+
+document.getElementById('form').addEventListener('submit', (e)=>{
+    e.preventDefault()
+    
+    if(other.value){
+        const body = {
+            name: userName.value,
+            whatsapp: tel.value,
+            email: email.value,
+            message: msg. value,
+            genre: other.value,
+            music: JSON.stringify(musicStyles),
+            sports: JSON.stringify(sportsModalities)
+        }
+        fetch('http://localhost:3003/users', {
+            method:'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body)
+        }).then(res => res.text()).then(data=>{
+            alert(data)
+        }).catch(e=>{
+            alert(e.message)
+        })
+        
+    }else{
+        const body = {
+            name: userName.value,
+            whatsapp: tel.value,
+            email: email.value,
+            message: msg.value,
+            genre: sexOrientation,
+            music: JSON.stringify(musicStyles),
+            sports: JSON.stringify(sportsModalities)
+        }
+        fetch('http://localhost:3003/users', {
+            method:'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body)
+        }).then(res => res.text()).then(data=>{
+            alert(data)
+        }).catch(e=>{
+            alert(e.message)
+        })
+    }
+})
